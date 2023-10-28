a92c1593625daa0b00b7fd25bdd95d8b9d067746
Fahrel <fahrel@students.undip.ac.id>
2023-10-28 19:09:03 +0700

    chapter 16: adding some meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Dafex Dashboard",
+    default: "Dafex Dashboard",
+  },
+  description: "Dafex Dashboard created using Next.js 14",
+  metadataBase: new URL("https://nextjs-14-pg.vercel.app/"),
+};
 
 export default function RootLayout({
   children,
